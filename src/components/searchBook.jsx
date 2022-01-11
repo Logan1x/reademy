0410dd1a1bbf9a1c6edf18a8367649424e98ee9a
Khushal Sharma <sharmakhushal78@gmail.com>
2022-01-11 16:47:46 +0530

    Feat: added load more button with api max requests

--- src/components/searchBook.jsx
+++ src/components/searchBook.jsx
@@ -1,17 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function BookSearch() {
   const [book, setBook] = useState("");
   const [bookData, setBookData] = useState([]);
+  const [showLoadMoreBtn, setShowLoadMoreBtn] = useState(false);
+  const [showOtherBookOption, setShowOtherBookOption] = useState(false);
 
-  const handleForm = async (e) => {
+  const handleForm = async (e, defaultSearchResults = 5) => {
     e.preventDefault();
     const bookAPI = "https://www.googleapis.com/books/v1/volumes?q=";
     const apiKey = "&key=AIzaSyDbhse2d4w_bbA_0LPmEMaatKr9o3ZeDfg";
+    const defaultResults = "&maxResults=" + defaultSearchResults;
     try {
-      const data = await axios.get(bookAPI + book + apiKey);
+      const data = await axios.get(bookAPI + book + apiKey + defaultResults);
       setBookData(data.data.items);
+      setShowLoadMoreBtn(true);
+      setShowOtherBookOption(true);
       console.log(data.data.items[4].volumeInfo.imageLinks);
     } catch (error) {
       console.log(error);
@@ -20,7 +25,6 @@ export default function BookSearch() {
 
   return (
     <div>
-      <h1>hello</h1>
       <form
         className="flex flex-col"
         onSubmit={(e) => {
@@ -47,18 +51,43 @@ export default function BookSearch() {
         {bookData.map((item) => {
           return (
             <div className="flex flex-col justify-center border-2 border-indigo-500 m-1">
-              {item.volumeInfo.imageLinks ? <img
+              {item.volumeInfo.imageLinks ? (
+                <img
                   src={item.volumeInfo.imageLinks.thumbnail}
                   alt={item.volumeInfo.title}
                   className="h-40 w-36"
-                /> : <p className="text-red-500">Image not found</p>
-              }
+                />
+              ) : (
+                <p className="text-red-500">Image not found</p>
+              )}
 
               <p className="w-36 py-1">{item.volumeInfo.title}</p>
             </div>
           );
         })}
+        {showOtherBookOption ? (
+          <div className="flex flex-col justify-center border-2 border-indigo-500 m-1">
+            <img src="" alt="book cover" className="h-40 w-36" />
+
+            <p className="w-36 py-1">other book</p>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
+      {showLoadMoreBtn ? (
+        <button
+          onClick={(e) => {
+            handleForm(e, 15);
+            setShowLoadMoreBtn(false);
+          }}
+          className="border px-2 py-1 bg-indigo-300"
+        >
+          Load More
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
